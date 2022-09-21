cdf6d82a60f3599b82b967680bcda5a90e101b27
felix barriere <felixbarriere@LAPTOP-GTUTFUJ1.localdomain>
2022-09-21 11:53:51 +0200

    start ex02

---
+++ CPP_00/ex02/Account.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "Account.hpp"
+
+/* Constructeur */
+
+Account::Account(int initial_deposit )
+{
+
+}
+
+Account::~Account(void)
+{
+
+}
+
+/* Accessors */
+
+int	Account::getNbAccounts(void)
+{
+	return (0);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (0);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (0);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (0);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	std::cout <<  "Test"  << std::endl;
+}
+
+/* fonctions membres */
+
+void		Account::makeDeposit(int initial_deposit )
+{
+
+}
+
+bool		Account::makeWithdrawal( int withdrawal )
+{
+
+}
+int			Account::checkAmount( void ) const
+{
+	return (0);
+}
+
+void		Account::displayStatus( void ) const
+{
+
+}
+
+void	Account::_displayTimestamp( void )
+{
+
+}
