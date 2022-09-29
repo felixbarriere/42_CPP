e298422de1a324359164796f4e3991b842565fda
felix barriere <felixbarriere@LAPTOP-GTUTFUJ1.localdomain>
2022-09-29 17:28:26 +0200

    ex02 done

---
+++ CPP_03/ex02/FragTrap.cpp
@@ -0,0 +1,44 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(std::string	name) : ClapTrap(name)
+{
+	this->setName(name); 
+	this->setHitPoints(100); 
+	this->setEnergyPoints(100); 
+	this->setAttackDamage(30);
+	std::cout << "constructor FragTrap called " << std::endl;
+}
+
+FragTrap::FragTrap( FragTrap&	copy) : ClapTrap(copy)
+{
+	std::cout << "copy constructor FragTrap called " << std::endl;
+	*this = copy;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "destructor FragTrap called " << std::endl;
+}
+
+/* Member functions */
+
+void	FragTrap::highFivesGuys(void)
+{
+		std::cout << "FragTrap " << this->getName() <<  " asks a high five!" << std::endl;
+
+}
+
+
+/* Operateurs */
+
+FragTrap&	FragTrap::operator=( FragTrap& rhs)
+{
+	std::cout << "FragTrap assignment operator called" << std::endl;
+
+	this->setName(rhs.getName());
+	this->setHitPoints(rhs.getHitPoints());
+	this->setEnergyPoints(rhs.getEnergyPoints());
+	this->setAttackDamage(rhs.getAttackDamage());
+
+	return (*this);
+}
---
+++ CPP_03/ex02/FragTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef FRAGTRAP_HPP
+#define	FRAGTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	public:
+		FragTrap(std::string	name);
+		FragTrap( FragTrap&	copy);
+		~FragTrap();
+
+		FragTrap&	operator=( FragTrap& rhs);
+
+		void		highFivesGuys(void);
+};
+
+#endif
