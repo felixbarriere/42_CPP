57bfebd5ac2a56c13a1977fc8df13136b8aa40a4
Felix BARRIERE-CONSTANTIN <fbarrier@e3r1p2.clusters.42paris.fr>
2022-10-18 16:32:46 +0200

    ex01 done

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -1,22 +1,16 @@
 #include "Data.hpp"
 
+/*  process of translating a data structure or object state into a format that can be stored 
+(for example, in a file or memory data buffer) or transmitted (for example, over a computer network)
+and reconstructed later (possibly in a different computer environment). */
+
 uintptr_t serialize(Data* ptr)
 {
-	unsigned int u;
-	u = reinterpret_cast<uintptr_t>(ptr);
-
-	std::cout << "test"<< std::endl;
-
-
-	return (u);
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data* deserialize(uintptr_t raw)
 {
-	// Data *data;
-	std::cout << "test2" << std::endl;
-
-	// data = reinterpret_cast<Data*>(raw);
 	return (reinterpret_cast<Data*>(raw));
 }
 
@@ -26,17 +20,17 @@ int main()
 
 	data.i = 42;
 	std::cout << "data: " << data.i << std::endl;
+	std::cout << "data adress: " << &data << std::endl;
 	
 	uintptr_t u = serialize(&data);
 
 	data = *deserialize(u);
-	std::cout << "test3"<< std::endl;
-
-	// Data	*data2 = deserialize(u);
 
 	std::cout << "data: " << data.i << std::endl;
+	std::cout << "data adress: " << &data << std::endl;
 
-	(void)u;
-	// (void)data2;
+	Data data2 = *deserialize(u);
+	std::cout << "data2 adress: " << &data2 << std::endl;
+
 	return (0);
 }
