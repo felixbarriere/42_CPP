b7682415bbfa8d6cc806420cfd891d809499585e
felix barriere <felixbarriere@LAPTOP-GTUTFUJ1.localdomain>
2022-10-25 12:04:50 +0200

    longestSpan ok

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -1,27 +1,63 @@
 #include "utils.hpp"
 
-
-
 int main()
 {
-	// std::list<int>	array[] = {2, 42, 7, 9};
-	std::vector<int>	array;
-	
-	array.push_back(2);
-	array.push_back(42);
-	array.push_back(7);
-	array.push_back(9);
-	
-	try
 	{
-		easyfind(array, 10);
-		std::cout << "TRUE" << std::endl;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+		std::cout << std::endl << " \e[0;33m****** Test element in list ******\e[0m " << std::endl << std::endl;
 
+		std::vector<int>	array;
 		
+		array.push_back(2);
+		array.push_back(42);
+		array.push_back(7);
+		array.push_back(9);
+		try
+		{
+			easyfind(array, 10);
+			std::cout << "TRUE" << std::endl;
+		}
+		catch (const NotInList& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << std::endl << " \e[0;33m****** Test element not in list ******\e[0m " << std::endl << std::endl;
+
+		std::vector<int>	array;
+		
+		array.push_back(2);
+		array.push_back(42);
+		array.push_back(7);
+		array.push_back(9);
+		try
+		{
+			easyfind(array, 42);
+			std::cout << "TRUE" << std::endl;
+		}
+		catch (const NotInList& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << std::endl << " \e[0;33m****** Test element double not in list ******\e[0m " << std::endl << std::endl;
+
+		std::vector<double>	array;
+		
+		array.push_back(2.42);
+		array.push_back(42.42);
+		array.push_back(7.98);
+		array.push_back(9.987654);
+		try
+		{
+			easyfind(array, 42);
+			std::cout << "TRUE" << std::endl;
+		}
+		catch (const NotInList& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 	return (0);
 }
--- CPP_08/ex00/utils.hpp
+++ CPP_08/ex00/utils.hpp
@@ -8,9 +8,13 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <list>
 
 // exception et message
-
+class NotInList : public std::exception
+{ public: virtual const char*	what() const throw()
+	{ return ("The element requested is not in the list ");  }
+};
 
 template <typename T>
 bool	easyfind( T container, int toFind)
@@ -18,7 +22,7 @@ bool	easyfind( T container, int toFind)
 	if (std::find(container.begin(), container.end(), toFind) != container.end())
 		return (TRUE);
 	else
-		throw std::exception();
+		throw NotInList();
 }
 
 #endif
